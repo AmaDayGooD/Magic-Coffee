4309b257f98c2a9933f548fe303adff44e04c13e
Aleksandr <sasha.schibaew@yandex.ru>
2023-07-05 00:30:02 +0300

    первый экран

---
+++ app/src/main/java/com/example/magiccoffe/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.magiccoffe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.magiccoffe.screens.MainScreen
+import com.example.magiccoffe.ui.theme.MagicCoffeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MagicCoffeTheme {
+                    MainScreen()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/magiccoffe/screens/MainScreen.kt
@@ -0,0 +1,54 @@
+package com.example.magiccoffe.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.magiccoffe.R
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreen(){
+    Image(painter = painterResource(id = R.drawable.backgroundlogo),
+        contentDescription = "img", modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+        )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            //
+            .background(color = Color(29, 35, 53, 94)),
+        contentAlignment = Alignment.Center,
+
+    ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "logo",
+                alignment = Alignment.BottomCenter,
+                modifier = Modifier.padding(bottom = 200.dp)
+            )
+        Text(
+            text = "Magic Coffee",
+            fontSize = 64.sp,
+            color = Color.White,
+            fontFamily = FontFamily(Font(R.font.reeniebeanie)),
+            fontWeight = FontWeight(10),
+            modifier = Modifier.padding(bottom = 110.dp)
+        )
+
+    }
+}
